ac795760bc4d4e2398793dc977b7dee345c88cfe
Ben Buaron <ben.buaron1@gmail.com>
2022-03-06 15:30:37 +0200

    Update settings.py

--- LaptopStore/settings.py
+++ LaptopStore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@=m42$=65e9p@h8bbf&nawsw%61tg+#bkyv#mo(exlfxy@9756'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
